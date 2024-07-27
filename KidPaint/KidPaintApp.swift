29d2bb8d559351ef10d2531f18e8c54f0d8aaaff
Renning Bruns <ren@renmail.net>
2024-07-27 15:42:56 -0700

    Initial Commit

---
+++ KidPaint/KidPaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidPaintApp.swift
+//  KidPaint
+//
+//  Created by Renning Bruns on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct KidPaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
